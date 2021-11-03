798d4d414cd68b9186662a2b954bd2484c0f2853
Tanja <tanja120a@gmail.com>
2021-11-03 22:53:46 +0100

    javascript version with no initial tasks

--- js/data.js
+++ js/data.js
@@ -8,13 +8,14 @@ class Task{
     }
 }
 class Board{
-    constructor(name, className, tasks,id ){
+    /*constructor(name, className, tasks,id ){
         this.name = name;
         this.className = className;
         this.tasks = tasks;
         this.id =  id;
-    }
+    }*/
 
+ 
     static displayBoard(board){
         document.querySelector(".board__wrapper").innerHTML = 
          board.map(el => {
@@ -50,27 +51,45 @@ class Board{
 
 /**Initial Values */
 
-let initialTasks = [["release the course", 0], ["sit back and relax", 0], ["work on projects", 1],
+/*let initialTasks = [["release the course", 0], ["sit back and relax", 0], ["work on projects", 1],
  ["listen music", 1], ["being cool", 2], ["being uncool", 2], ["getting stuff done", 3],
   ["finished", 3]];
 let columns = [["Backlog", "backlog"], ["In Progress", "in-progress"], ["Complete", "complete"], ["On Hold", "on-hold"]];
+*/
 
 /**OnLoad Function */
 
 let myBoard =  function (){   
+    /**
+     * let myBoard =  function (){   
     let tasks = [];
     let board = [];
 
-    //Get from LS Tasks and Board and comment initial 
     initialTasks.forEach((el, index) => tasks.push(new Task(el[0],index, el[1] )));
     columns.forEach((el, index)=> board.push(new Board(el[0], el[1],tasks.filter( t => t.column == index ) , index)));
     Board.displayBoard(board);
-
     localStorage.setItem("board",JSON.stringify(board));
     board = JSON.parse( localStorage.getItem('board'));
     localStorage.setItem("tasks", JSON.stringify(tasks));
     tasks = JSON.parse(localStorage.getItem('tasks'));
 
+     */
+
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    let board = JSON.parse( localStorage.getItem('board')) ||[];
+     
+    let initialBoard = [{name: "Backlog", className: "backlog", tasks: [], id : 0},
+     {name: "In Proggres", className: "in-progress", tasks: [], id : 1}, 
+     {name: "Complete", className: "complete", tasks: [], id : 2}, 
+     {name: "On Hold", className: "on-hold", tasks: [], id : 3}];
+
+    if(board.length !== 0){
+        Board.displayBoard(board);
+    }else{
+        board = initialBoard;
+        localStorage.setItem("board",JSON.stringify(board));
+        localStorage.setItem("tasks", JSON.stringify(tasks))
+    }
  //Add Event Listeners   
     const draggableItems = document.querySelectorAll(".item");
     draggableItems.forEach((item) => {
